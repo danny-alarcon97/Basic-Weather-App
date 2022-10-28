6ffaf615db58c094a7f999c75ee376b2fa9f9a02
daniel <danny.alarcon1997@gmail.com>
2022-10-28 11:54:09 -0400

    learning Parse JSON

--- app.js
+++ app.js
@@ -6,9 +6,24 @@ const port = 3000;
 
 app.get("/", function (req, res) {
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=London&appid=1c4cdcce8878d7e83f7cef5765e50362&units=metric";
+    "https://api.openweathermap.org/data/2.5/weather?q=Yonkers&appid=1c4cdcce8878d7e83f7cef5765e50362&units=imperial";
+
   https.get(url, function (response) {
-    console.log(response);
+    console.log(response.statusCode);
+
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      console.log(temp);
+      console.log(weatherDescription);
+
+      // const object = {
+      //   name: "Daniel",
+      //   favoriteFood: "Pizza",
+      // };
+      // console.log(JSON.stringify(object));
+    });
   });
 
   res.send(`Server is up and running`);
